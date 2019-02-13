88f2f3aab4a2426974991dae0fa4c27752138053
Anjia Wang <anjiawang@gmail.com>
2019-02-12 23:21:53 -0500

    Update CUDA scripts for Fornax.

--- fornax/fetch-cuda.sh
+++ fornax/fetch-cuda.sh
@@ -1,24 +1,12 @@
 #!/bin/bash
 
-## Base install (~1.2 GB)
-#wget https://developer.nvidia.com/compute/cuda/9.1/Prod/local_installers/cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64 > cuda-repo-ubuntu1604-9-1-local_9.1.85-1_amd64
-#
-## Patch 1 (~97.4 MB)
-#wget https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/1/cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-1_1.0-1_amd64 > cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-1_1.0-1_amd64
-#
-## Patch 2 (~16.5 MB)
-#wget https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/2/cuda-repo-ubuntu1604-9-1-local-compiler-update-1_1.0-1_amd64 > cuda-repo-ubuntu1604-9-1-local-compiler-update-1_1.0-1_amd64
-#
-## Patch 3 (~97.4 MB)
-#wget https://developer.nvidia.com/compute/cuda/9.1/Prod/patches/3/cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-3_1.0-1_amd64 > cuda-repo-ubuntu1604-9-1-local-cublas-performance-update-3_1.0-1_amd64
 
+# Base install (~1.9 GB)
+wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-rhel7-10-0-local-10.0.130-410.48-1.0-1.x86_64 
 
-
-# Base install (~1.2 GB)
-wget https://developer.nvidia.com/compute/cuda/10.0/Prod/local_installers/cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64 
 
 # Rename raw files to have correct extension.
 for f in cuda*; do
-    mv "${f}" "${f}".deb
+    mv "${f}" "${f/x86_64/x86_64.rpm}"
 done;
 
--- fornax/install-cuda.sh
+++ fornax/install-cuda.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-sudo dpkg -i cuda-repo-ubuntu1804-10-0-local-10.0.130-410.48_1.0-1_amd64.deb 
+sudo rpm -i cuda-repo-rhel7-10-0-local-10.0.130-410.48-1.0-1.x86_64.rpm 
+sudo yum clean all
+sudo yum install cuda
 
-sudo apt-key add /var/cuda-repo-10-0-local-10.0.130-410.48/7fa2af80.pub
-
-sudo apt update
 # Install CUDA, without the lightdm windowing system.
 # This cuts down our deps to just X11, instead of full Desktop Ubuntu.
-sudo apt install cuda lightdm-
+# sudo apt install cuda lightdm-
+
+# Patch
